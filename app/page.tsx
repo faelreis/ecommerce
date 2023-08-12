5cab9e60b6e6b75df8764fc7a2c1e64c6d4e0e02
Rafael Reis Franco <rafaelreisfranco15@gmail.com>
2023-08-12 10:03:39 -0300

    Product search

--- app/page.tsx
+++ app/page.tsx
@@ -16,20 +16,22 @@ interface Props {
     color?: string;
     category?: string;
     size?: string;
+    search?: string;
   }
 }
 
 export default async function Page({ searchParams }: Props)  {
-  const { date = 'desc', price, color, category, size } = searchParams
+  const { date = 'desc', price, color, category, size, search } = searchParams
   const priceOrder = price ? `| order(price ${price})` : ''
   const dateOrder = date ? `| order(_createAt ${date})` : ''
   const order = `${priceOrder}${dateOrder}`
 
-  const productFilter = `_type == 'product'`
-  const colorFilter = color ? `&& '${color}' in colors` : ''
-  const categoryFilter = category ? `&& '${category}' in categories` : ''
-  const sizeFilter = size ? `&& '${size}' in sizes` : ''
-  const filter = `*[${productFilter}${colorFilter}${categoryFilter}${sizeFilter}]`
+  const productFilter = `_type == 'product'`;
+  const colorFilter = color ? `&& '${color}' in colors` : '';
+  const categoryFilter = category ? `&& '${category}' in categories` : '';
+  const sizeFilter = size ? `&& '${size}' in sizes` : '';
+  const searchFilter = search ? `&& name match "${search}"` : '';
+  const filter = `*[${productFilter}${colorFilter}${categoryFilter}${sizeFilter}${searchFilter}]`;
 
   const products = await client.fetch<SanityProduct[]>(
       groq`${filter} ${order}{
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -11,15 +11,25 @@ import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
 
 export function SiteHeader() {
+  const router = useRouter();
   const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const defaultSearchParams = searchParams.get('search') ?? ''
 
   if(pathname.startsWith('/studio')) return null;
+
+  function onSubmit(event: React.SyntheticEvent<HTMLFormElement>){
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget)
+    const searchQuery = formData.get('search')
+    router.replace(`/?search=${searchQuery}`)
+  }
   
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="mx-auto flex h-16 max-w-6xl items-center justify-between space-x-4 px-6 sm:space-x-0">
         <MainNav />
-        <form className="hidden items-center lg:inline-flex">
+        <form onSubmit={onSubmit} className="hidden items-center lg:inline-flex">
           <Input
             id="search"
             name="search"
@@ -27,6 +37,7 @@ export function SiteHeader() {
             autoComplete="off"
             placeholder="Search products..."
             className="h-9 lg:w-[300px]"
+            defaultValue={defaultSearchParams}
           />
         </form>
         <div className="flex items-center space-x-1">
